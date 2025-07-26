aac7cef69c2739f76c40d8ce86aafea62a23716b
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-07-26 12:25:57 +0530

    Added

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -2,8 +2,13 @@
 	let { children } = $props();
 </script>
 
-<section class="py-[60px]">
-	<div class="hms-container">
+<section class="">
+	<div class="hms-container relative">
+		<header class=" flex justify-end  relative -top-1">
+			<a href="/" class="flex h-[130px] w-auto">
+				<img src="/images/blue-stethoscope.png" alt="icon" class=" h-full w-auto" />
+			</a>
+		</header>
 		<div class="flex w-full items-center">
 			<div class=" border-r-[2px] border-[#d7d7d7] py-[87px] pr-[22px]">
 				<figure class="flex h-[342px] w-[427px] items-center justify-center">
--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,32 +1,81 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
 
+	let showForm = $state(false);
+	let loading = $state(false);
+	let show_password = $state(false);
+	let type = $derived(show_password ? 'text' : 'password');
 
+	$effect(() => {
+		setTimeout(() => {
+			showForm = true;
+		}, 1);
+	});
+</script>
 
-<section>
-    <h1 class=" text-primary font-bold text-3xl mb-[18px]">Sign in to your account.</h1>
-    <p class=" text-primary text-xl ">
-        Don't have an account? <a href="/auth/signup" class=" font-semibold text-secondary underline hover:text-error">Sign Up</a>
-    </p>
+<section class="overflow-x-hidden">
+	{#if showForm}
+		<div class="w-full" in:fly={{ x: -325, duration: 500 }}>
+			<h1 class=" mb-[18px] text-3xl font-bold text-primary">Sign in to your account.</h1>
+			<p class=" text-xl text-primary">
+				Don't have an account? <a
+					href="/auth/signup"
+					class=" font-semibold text-secondary underline hover:text-error">Sign Up</a
+				>
+			</p>
 
+			<div class="mt-[33px]">
+				<form action="">
+					<div class="grid w-full grid-cols-1 gap-4">
+						<label>
+							<input name="email" type="email" placeholder="Email" class="input w-full" />
+						</label>
 
-    <div class="mt-[33px]">
-        <form action="">
-            <div class="w-full grid grid-cols-1 gap-4">
-                <label>
-                    <input name="email" type="email" placeholder="Email" class="input w-full" />
-                </label>
+						<label class=" relative">
+							<input name="password" {type} placeholder="Password" class="input w-full" />
+							<button
+								type="button"
+								class="absolute top-[10px] right-[10px] z-[1] cursor-pointer"
+								onclick={() => (show_password = !show_password)}
+							>
+								{#if show_password}
+									<!-- Hidden Eye (shown when password is visible) -->
+									<svg
+										class="h-5 w-5 fill-[#d7d7d7] transition-colors hover:fill-[#aaaaaa]"
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 18 16"
+									>
+										<path
+											d="M 7.505 3.667 C 7.967 3.482 8.47 3.368 8.998 3.368 C 11.256 3.368 13.088 5.254 13.088 7.578 C 13.088 8.122 12.982 8.64 12.798 9.116 L 15.19 11.579 C 16.425 10.518 17.399 9.145 18 7.578 C 16.581 3.882 13.092 1.263 8.998 1.263 C 7.853 1.263 6.757 1.474 5.738 1.853 L 7.505 3.667 Z M 1.861 0 L 0.818 1.074 L 2.683 2.994 L 3.055 3.377 C 1.706 4.463 0.638 5.907 0 7.578 C 1.415 11.276 4.908 13.895 8.998 13.895 C 10.266 13.895 11.476 13.642 12.585 13.183 L 12.933 13.541 L 15.317 16 L 16.36 14.931 L 1.861 0 Z M 4.908 7.578 C 4.908 6.914 5.072 6.291 5.342 5.726 L 6.606 7.027 C 6.569 7.208 6.544 7.389 6.544 7.578 C 6.544 8.973 7.644 10.105 8.998 10.105 C 9.182 10.105 9.358 10.08 9.53 10.042 L 10.794 11.343 C 10.25 11.621 9.645 11.789 8.998 11.789 C 6.74 11.789 4.908 9.903 4.908 7.578 Z M 8.998 5.053 L 8.863 5.065 L 11.44 7.718 L 11.452 7.578 C 11.452 6.185 10.352 5.053 8.998 5.053 Z"
+											fill-rule="nonzero"
+										/>
+									</svg>
+								{:else}
+									<!-- Visible Eye (shown when password is hidden) -->
+									<svg
+										class="h-5 w-5 fill-[#d7d7d7] transition-colors hover:fill-[#aaaaaa]"
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 20 14"
+									>
+										<path
+											d="M 20 7 C 18.4 2.9 14.5 0 10 0 C 5.5 0 1.6 2.9 0 7 C 1.6 11.1 5.5 14 10 14 C 14.5 14 18.4 11.1 20 7 Z M 14.5 7 C 14.5 9.6 12.5 11.7 10 11.7 C 7.5 11.7 5.5 9.6 5.5 7 C 5.5 4.4 7.5 2.3 10 2.3 C 12.5 2.3 14.5 4.4 14.5 7 Z M 12.7 7 C 12.7 5.5 11.5 4.2 10 4.2 C 8.5 4.2 7.3 5.5 7.3 7 C 7.3 8.5 8.5 9.8 10 9.8 C 11.5 9.8 12.7 8.5 12.7 7 Z"
+											fill-rule="nonzero"
+										/>
+									</svg>
+								{/if}
+							</button>
+						</label>
 
-                <label>
-                    <input name="password" type="password" placeholder="Password" class="input w-full" />
-                </label>
+						<button type="button" class=" btn text-lg font-bold btn-primary"> Sign In </button>
+					</div>
+				</form>
 
-                <button type="button" class=" btn btn-primary font-bold text-lg">
-                    Sign In
-                </button>
-            </div>
-        </form>
-
-        <p class="mt-[32px]">
-            <a href="/" class=" text-xl font-bold text-secondary hover:underline">Forgot your password?</a>
-        </p>
-    </div>
+				<p class="mt-[32px]">
+					<a href="/" class=" text-xl font-bold text-secondary hover:underline"
+						>Forgot your password?</a
+					>
+				</p>
+			</div>
+		</div>
+	{/if}
 </section>
