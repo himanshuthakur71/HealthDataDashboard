4bc0eb3f2f56ab39f5897efbafe11ae4a1a77622
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-07-26 11:47:34 +0530

    Added

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<header class="w-full bg-base-100 shadow-sm">
+	<div class="hms-container">
+		<div class="navbar">
+			<div class="navbar-start">
+				<a href="/" class="btn text-xl btn-ghost">HealthDash</a>
+			</div>
+
+			<div class="navbar-end">
+				<a href="/auth/login" class="btn">Login</a>
+			</div>
+		</div>
+	</div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
+	import 'nprogress/nprogress.css';
 	import '../app.css';
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/state';
 
 	let { children } = $props();
+
+	// Initialize NProgress configuration
+	NProgress.configure({
+		minimum: 0.16,
+		showSpinner: true
+	});
+
+	$effect(() => {
+		// Start NProgress when navigating starts
+		if (navigating?.to) {
+			NProgress.start();
+		} else {
+			NProgress.done();
+		}
+	});
 </script>
 
-{@render children()}
+<Navbar />
+
+<main>
+	{@render children()}
+</main>
