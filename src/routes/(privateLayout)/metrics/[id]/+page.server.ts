069c89b3f5e8323521add5b393dda0f4975cc9d2
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-07-26 21:56:59 +0530

    acess added

--- src/routes/(privateLayout)/metrics/[id]/+page.server.ts
+++ src/routes/(privateLayout)/metrics/[id]/+page.server.ts
@@ -1,22 +1,26 @@
 import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
+
 
 export const load: PageServerLoad = async ({ params, locals: { supabase, user } }) => {
 	const id = params.id;
 
 	if (!user) {
-		return { metric: null };
+		throw error(422, 'Access denied');
 	}
 
-	const { data: metric, error } = await supabase
+	const { data: metric, error: err } = await supabase
 		.from('health_metrics')
 		.select('*')
 		.eq('id', id)
 		.eq('user_id', user.id)
 		.single();
 
-	if (error) {
-		console.error('Error fetching metric:', error);
-		return { metric: null };
+
+	if (err) {
+		console.error('Error fetching metric:', err);
+
+		throw error(422, 'Access denied');
 	}
 
 	return { metric };
