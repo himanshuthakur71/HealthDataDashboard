ff3715295625a609e8a412ac77d91c69a950d935
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-07-27 09:51:09 +0530

    Html added

---
+++ src/routes/(privateLayout)/reports/[id]/+page.server.ts
@@ -0,0 +1,25 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params, locals: { supabase, user } }) => {
+    const { id } = params;
+
+    if (!user) {
+        throw error(422, 'Access denied');
+    }
+
+
+    const { data: report, error: err } = await supabase
+        .from('ai_reports')
+        .select('*')
+        .eq('id', id)
+        .eq('user_id', user.id)
+        .single();
+
+    if (err) {
+        console.error('Error loading report:', err.message);
+        throw error(422, 'Access denied');
+    }
+
+    return { report };
+};
---
+++ src/routes/(privateLayout)/reports/[id]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { marked } from 'marked';
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
+<section class="hms-container py-6">
+	<div class="mb-8">
+		<h2 class="mb-2 text-3xl font-semibold text-gray-800">🩺 Doctor's AI Report</h2>
+		<p class="text-gray-600">Generated on {new Date(data?.report.created_at).toLocaleString()}</p>
+		<p><strong>Health Score:</strong> {data?.report.health_score}</p>
+	</div>
+
+	<div class="w-full">
+		{@html marked(data?.report.feedback)}
+	</div>
+
+	<div class="mt-16 w-full">
+		<a href="/reports" class=" btn btn-outline btn-lg btn-primary">Back</a>
+	</div>
+</section>
