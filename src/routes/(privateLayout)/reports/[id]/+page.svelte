35b9bdbde265861de328fb2855f8e495bc16c163
himanshuthakur71 <49717350+himanshuthakur71@users.noreply.github.com>
2025-07-27 09:56:05 +0530

    client Side Print Added

--- src/routes/(privateLayout)/reports/[id]/+page.svelte
+++ src/routes/(privateLayout)/reports/[id]/+page.svelte
@@ -3,6 +3,10 @@
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+
+	function printReport() {
+		window.print();
+	}
 </script>
 
 <section class="hms-container py-6">
@@ -12,11 +16,20 @@
 		<p><strong>Health Score:</strong> {data?.report.health_score}</p>
 	</div>
 
-	<div class="w-full">
+	<div class="w-full mb-6 flex justify-end md:mt-[-60px] print:hidden">
+		<button
+			onclick={printReport}
+			class=" btn btn-secondary btn-sm "
+		>
+			Print as PDF
+		</button>
+	</div>
+
+	<div class="w-full border-t border-[#d7d7d7] pt-4">
 		{@html marked(data?.report.feedback)}
 	</div>
 
-	<div class="mt-16 w-full">
+	<div class="mt-16 w-full print:hidden">
 		<a href="/reports" class=" btn btn-outline btn-lg btn-primary">Back</a>
 	</div>
 </section>
